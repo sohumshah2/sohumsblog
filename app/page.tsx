02ea56ab7806bddf0cd371e49a4beef9155d7f60
Sohum <sohumshah2@gmail.com>
2024-02-09 17:38:05 +1100

    made root page redirect to /blogs

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <div>Hello</div>
+  redirect("/blogs");
 }
